e1df98c0c5c85ae6cba0a58f29faecb37bcf2e3d
Pordrack <50073154+Pordrack@users.noreply.github.com>
2022-04-02 18:05:35 +0200

    Revert "Merge remote-tracking branch 'origin/diriann-branch'"
    This reverts commit 35353e6dfe7c64a902462221fef9f0a731b1da91, reversing
    changes made to e9ae25876912b0f44ce7a3734e14883050b29bf2.

--- Assets/Scripts/PlantScript.cs
+++ Assets/Scripts/PlantScript.cs
@@ -1,28 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PlantScript : MonoBehaviour
 {
     private float initialWaterLevel;
     public float WaterLevel = 10;
-    
-    public Image WaterBar;
-    float WaterBarY = -3;
     public float MaxWaterLevel = 15;
     public SpriteRenderer plantSpriteRenderer;
     public Sprite[] Images; //Les images representant les differents niveaux de secheresse de la plante.
     public Sprite DeathImage; //L'image de la plante morte
     private bool IsDead = false;
 
+    // Start is called before the first frame update
     void Start()
     {
         initialWaterLevel = WaterLevel;
-        WaterBar.transform.position = new Vector3(transform.position.x, transform.position.y + WaterBarY, transform.position.z);
-        WaterBar.transform.position = new Vector3(transform.position.x, transform.position.y + WaterBarY, transform.position.z);
 
-        initialWaterLevel = WaterLevel;
     }
 
     // Update is called once per frame
@@ -35,12 +29,7 @@ public class PlantScript : MonoBehaviour
 
         //On pert de l'eau en fonction du climat
         WaterLevel -= Time.deltaTime * ClimateScript.Instance.DrynessRate;
-        WaterBarFiller();
-    }
 
-    void WaterBarFiller() //remplissage visible de la jauge d'eau
-    {
-        WaterBar.fillAmount = WaterLevel / MaxWaterLevel;
         if (WaterLevel <= 0)
         {
             Die();
--- Assets/Scripts/WaterCanScript.cs
+++ Assets/Scripts/WaterCanScript.cs
@@ -1,20 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class WaterCanScript : MonoBehaviour
 {
     private Vector3 mousePosition;
     public GameObject particleSystem;
-
     public float WaterReserve = 40;
-    public float WaterMaxReserve = 100;
     public float WaterPerSecond = 5;
-    public Image WaterBar;
-    float WaterBarY = 1f;
     public LayerMask layerMask;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -24,18 +18,10 @@ public class WaterCanScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(WaterReserve + " water left in can");
-
-        //l'objet suit les mouvements de la souris
         mousePosition = Input.mousePosition;
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
         mousePosition.z = 0;
         transform.position = mousePosition;
-
-        //la jauge d'eau suit les mouvements de la souris
-        WaterBar.transform.position = new Vector3(mousePosition.x, mousePosition.y + WaterBarY, mousePosition.z);
-
-        WaterBarFiller();
 
         particleSystem.SetActive(Input.GetMouseButton(0) && WaterReserve > 0);
         if (!Input.GetMouseButton(0))
@@ -58,10 +44,5 @@ public class WaterCanScript : MonoBehaviour
                 this.WaterReserve -= WaterPerSecond * Time.deltaTime;
             }   
         }
-    }
-
-    void WaterBarFiller() //remplissage visible de la jauge d'eau
-    {
-        WaterBar.fillAmount = WaterReserve / WaterMaxReserve;
     }
 }
